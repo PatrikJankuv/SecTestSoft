494d4c7ba1c6a229886142c05afd6f63fad48565
Patrik Jankuv <jankuv.patri@outlook.com>
2022-11-14 21:36:44 +0100

    Initial commit

---
+++ src/test/java/lab/service/LoginsTest.java
@@ -0,0 +1,15 @@
+package lab.service;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class LoginsTest extends TestCase {
+
+    @Test
+    public void testDoGet() {
+    }
+
+    @Test
+    public void testDoPost() {
+    }
+}
